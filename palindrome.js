72925111c563b84e016c16584534602b320455a4
Teodor Coste <155558856+teoc22@users.noreply.github.com>
2024-05-21 10:55:50 +0300

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+// Problem: Write a function that checks if a given string is a palindrome.
+// Example: 'isPalindrome ('racecar') should return `true`.
+
+function isPalindrome(nonReversed) {
+    reversed = "";
+   for(let i = nonReversed.length - 1; i >= 0; i--) {
+    reversed = reversed + nonReversed[i];
+   }
+   
+    if(reversed == nonReversed) {
+        return true;
+        } else { 
+        return false;
+   }    
+}
+
+   function init() {
+    uiElement = document.getElementById("exercise");
+    uiElement.innerHTML = isPalindrome("racecar")
+   }
+
+   init();
