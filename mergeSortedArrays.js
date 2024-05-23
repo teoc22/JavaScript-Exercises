069c1337fe4c14f9fce6b36f5d25bbd11668e467
Teodor Coste <155558856+teoc22@users.noreply.github.com>
2024-05-23 15:47:32 +0300

    Add files via upload

---
+++ mergeSortedArrays.js
@@ -0,0 +1,21 @@
+// Problem: Write a function that merges two sorted arrays into a single sorted array.
+// Example: `mergeArrays([1, 3, 5], [2, 4, 6])` should return `[1, 2, 3, 4, 5, 6]`.
+
+function mergeArrays(array1, array2) {
+    let groupedArray = [];
+        
+    for (let i = 0; i < array1.length; i = i++) {
+        groupedArray = array1.concat(array2);
+        groupedArray.sort(); 
+
+    console.log(groupedArray);
+    return groupedArray;
+}
+}
+
+function init() {
+    let uiElement = document.getElementById("exercise");
+    uiElement.innerHTML = mergeArrays([1, 3, 5], [2, 4, 6]);
+}
+
+init();
