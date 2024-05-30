908e9519a0cb483dfc4c59bc2840b3a980964daf
Teodor Coste <155558856+teoc22@users.noreply.github.com>
2024-05-30 15:18:59 +0300

    Create sumArray.js

---
+++ sumArray.js
@@ -0,0 +1,35 @@
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// Examples
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+// Assumptions
+// You can assume that you are only given numbers.
+// You cannot assume the size of the array.
+// You can assume that you do get an array and if the array is empty, return 0.
+
+function sum(arr) {
+  
+   // Initialize suma with 0
+    let suma = 0;
+
+    // Check if the array is empty
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    // Loop through each element in the array
+    for (let i = 0; i < arr.length; i++) {
+        suma = suma + arr[i];
+    }
+
+    // Return the computed sum
+    return suma;
+}
