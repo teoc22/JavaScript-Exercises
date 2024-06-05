5ed08cd1169e74de2b6eaf6e09e2a2c8329a4e49
Teodor Coste <155558856+teoc22@users.noreply.github.com>
2024-06-05 08:20:12 +0300

    Update validatePIN.js

--- 7 kyu/validatePIN.js
+++ 7 kyu/validatePIN.js
@@ -7,15 +7,16 @@
 // "12345"  -->  false
 // "a234"   -->  false
 
-function validatePIN (pin) {
+function validatePIN(pin) {
     // Check if the length of the pin is either 4 or 6
     if(pin.length === 4 || pin.length === 6) {
         // Variable to keep track if all characters are numbers
         let isNumber = true;
+
         // Loop through each character in the pin
         for(i = 0; i <= pin.length - 1; i++) {
             // Check if the current character is not a number
-            if (isNaN(Number(pin[i]))) {
+            if (isNaN(parseInt(pin[i]))) {
                 // If a non-number is found, set isNumber to false and break the loop
                 isNumber = false;
                 break;
